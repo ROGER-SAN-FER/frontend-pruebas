b100115558c7940082a8b23ec0da8c675407a943
Roger <roger190892@gmail.com>
2025-08-09 18:11:04 +0200

    Cambio de servidor backend a railway

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,3 @@
 // src/apiConfig.js
-export const API_BASE_URL = "https://backend-restaurant-x4sk.onrender.com/api";
+//export const API_BASE_URL = "https://backend-restaurant-x4sk.onrender.com/api";
+export const API_BASE_URL = "https://backend-restaurant-production-9a85.up.railway.app/api";
